61a94362b6824df5f2582e8f5b9b126d3243515c
Kazem Qulian <kgholian@gamil.com>
2023-08-05 10:07:35 +0330

    fix auth bug

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,8 +1,8 @@
 import Lottie from "lottie-react";
 import login from "../../lottie/login.json";
 import { styled } from "styled-components";
-import { useEffect } from "react";
-import { GoogleOAuthProvider, GoogleLogin } from "@react-oauth/google";
+import { GoogleOAuthProvider } from "@react-oauth/google";
+import LoginButton from "./LoginButton";
 
 const Box = styled.div`
   height: calc(100% - 30px);
@@ -15,36 +15,12 @@ const Box = styled.div`
 `;
 
 export default function Login() {
-
-  //   function handlecallbackResponse(response) {
-  //     console.log(response.credential);
-  //   }
-
-  // useEffect(() => {
-  //   /*global google*/
-  //   google.accounts.id.initialize({
-  //     client_id:
-  //       "222490645159-0a27ut476gfa7bqd7l0ceqku45k2sij6.apps.googleusercontent.com",
-  //     callback: handlecallbackResponse,
-  //   });
-
-  //   google.accounts.id.renderButton(document.getElementById("signIn"), {
-  //     scope: "profile email",
-  //     width: 240,
-  //     height: 50,
-  //     longtitle: true,
-  //     theme: "dark",
-  //     onsuccess: () => console.log("success"),
-  //     onfailure: () => console.log("failed"),
-  //   });
-  // }, []);
-
   return (
-    <Box>
-      <Lottie animationData={login} />
-      <GoogleOAuthProvider clientId="344789065811-vofbb91dbudn4hvvu6vvaqois5q77k66.apps.googleusercontent.com">
-        <GoogleLogin />  
-      </GoogleOAuthProvider>
-    </Box>
+    <GoogleOAuthProvider clientId="952237121300-n2bq52tfpa4csif128kknt43fj12vj24.apps.googleusercontent.com">
+      <Box>
+        <Lottie animationData={login} />
+        <LoginButton />
+      </Box>
+    </GoogleOAuthProvider>
   );
 }
